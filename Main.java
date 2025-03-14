5d9eb82a2c71eb548caacd1aca2c0186171d0ac2
PLChandrasekara <pramudilanka289@gmail.com>
2025-03-14 18:08:17 +0530

    print numbers 1 to 10

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String args[]){
+        //Print a message
         System.out.println("Hello World..!");
+
+        //print numbers 1 to 10
+        for(int i=1; i<=10; i++){
+            System.out.print(i+" ");
+        }
     }
 }
